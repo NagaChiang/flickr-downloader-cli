ccb10aa9dc518289bba674cd23df4d47bd3aa3fe
Naga <nagachiangdev@gmail.com>
2020-02-09 18:18:48 +0800

    Use mkdirp-promise instead of mkdirp

--- src/services/PhotoService.js
+++ src/services/PhotoService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import config from 'config';
-import mkdirp from 'mkdirp';
+import mkdirp from 'mkdirp-promise';
 import fs from 'fs';
 import sanitize from 'sanitize-filename';
 
